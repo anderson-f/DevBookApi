c337a66cf8c8877cc41aabe65417073cb2f0dbca
Anderson Freitas <andersonfreitas.tec@gmail.com>
2021-12-04 10:50:07 -0300

    feat: set up package config and database

--- api/main.go
+++ api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/router"
 	"fmt"
 	"log"
@@ -8,9 +9,10 @@ import (
 )
 
 func main() {
-	fmt.Println("Rodando API!")
+	config.Load()
+	fmt.Printf("Listening on port %d", config.Port)
 
 	r := router.Generate()
 
-	log.Fatal(http.ListenAndServe(":5000", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), r))
 }
---
+++ api/src/config/config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	// StringDatabaseConnection is the connection string with MySQL
+	StringDatabaseConnection = ""
+
+	// Port where the API will be running
+	Port = 0
+)
+
+// Load will initialize the environment variables
+func Load() {
+	var err error
+
+	if err = godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	Port, err = strconv.Atoi(os.Getenv("API_PORT"))
+
+	if err != nil {
+		Port = 9000
+	}
+
+	StringDatabaseConnection = fmt.Sprintf("%s:%s:@/%s?charset=utf&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+}
