8579bd7b80edc0090aa376e5843b868f1b090a98
Anderson Freitas <anderson.freitas@zoom.com.br>
2021-12-07 22:15:17 -0300

    feat: validate user fields and remove spaces

--- api/src/models/User.go
+++ api/src/models/User.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // User represents one user using social media
 type User struct {
@@ -10,4 +14,40 @@ type User struct {
 	Username  string    `json:"username,omitempty"`
 	Password  string    `json:"password,omitempty"`
 	CreatedAt time.Time `json:"CreatedAt,omitempty"`
+}
+
+// Prepare will call the methods for validation e format on user received
+func (user *User) Prepare() error {
+	if err := user.validate(); err != nil {
+		return err
+	}
+
+	user.cleanBorderSpaces()
+	return nil
+}
+
+func (user *User) validate() error {
+	if user.Name == "" {
+		return errors.New("O nome é obrigatório e não pode estar em branco")
+	}
+
+	if user.Username == "" {
+		return errors.New("O Username é obrigatório e não pode estar em branco")
+	}
+
+	if user.Email == "" {
+		return errors.New("O e-mail é obrigatório e não pode estar em branco")
+	}
+
+	if user.Password == "" {
+		return errors.New("O senha é obrigatório e não pode estar em branco")
+	}
+
+	return nil
+}
+
+func (user *User) cleanBorderSpaces() {
+	user.Name = strings.TrimSpace(user.Name)
+	user.Username = strings.TrimSpace(user.Username)
+	user.Email = strings.TrimSpace(user.Email)
 }
