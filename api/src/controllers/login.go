399159073c7841978783822cec914a38d130f55f
Anderson Freitas <andersonfreitas.tec@gmail.com>
2021-12-24 11:52:36 -0300

    feat: add jwt and create authentication package

--- api/src/controllers/login.go
+++ api/src/controllers/login.go
@@ -1,12 +1,14 @@
 package controllers
 
 import (
+	"api/src/authentication"
 	"api/src/database"
 	"api/src/models"
 	"api/src/repositories"
 	"api/src/responses"
 	"api/src/security"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 )
@@ -42,5 +44,7 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Você está logado! Parabéns!"))
+	token, _ := authentication.CreateToken(userFromDatabase.ID)
+	fmt.Println(token)
+	w.Write([]byte(token))
 }
